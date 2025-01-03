9b212b723fee7e0ea3444c0778bd8faf78d6cb06
Malaka <ithayg@gmail.com>
2025-01-03 22:32:11 +0200

    hello triangle

--- main.cpp
+++ main.cpp
@@ -49,9 +49,14 @@ int main(void) {
     glfwSetFramebufferSizeCallback(window, &framebuffer_size_callback);
 
     float vertices[] = {
-        -0.5, -0.5, 0.0,
-        0.5, -0.5, 0.0,
-        0.0, 0.5, 0.0
+        0.5, 0.5, 0.0,      // top right
+        0.5, -0.5, 0.0,     // bottom right
+        -0.5, -0.5, 0.0,    // bottom left
+        -0.5, 0.5, 0.0      // top left
+    };
+    u_int indices[] = {
+        0, 1, 3,    // first triangle
+        1, 2, 3     // second triangle
     };
 
     u_int VAO = 0;
@@ -62,6 +67,11 @@ int main(void) {
     glGenBuffers(1, &VBO);
     glBindBuffer(GL_ARRAY_BUFFER, VBO);
     glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+    u_int EBO = 0;
+    glGenBuffers(1, &EBO);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
 
     u_int vertexShader = glCreateShader(GL_VERTEX_SHADER);
     glShaderSource(vertexShader, 1, &vertexShaderSource, nullptr);
@@ -106,6 +116,11 @@ int main(void) {
     glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
     glEnableVertexAttribArray(0);
 
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
+    glBindVertexArray(0);
+
+    // wireframe mode
+    glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
 
     // render loop
     while (!glfwWindowShouldClose(window)) {
@@ -118,12 +133,18 @@ int main(void) {
 
         glUseProgram(shaderProgram);
         glBindVertexArray(VAO);
-        glDrawArrays(GL_TRIANGLES, 0, 3);
+        // glDrawArrays(GL_TRIANGLES, 0, 3);
+        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
 
         // check and call events and swap the buffers
         glfwSwapBuffers(window);
         glfwPollEvents();
     }
+
+    glDeleteVertexArrays(1, &VAO);
+    glDeleteBuffers(1, &VBO);
+    glDeleteBuffers(1, &EBO);
+    glDeleteProgram(shaderProgram);
 
     glfwTerminate();
 
